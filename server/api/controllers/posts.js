7fde7498f002c2ab0f2871c387133e9a8e5701ae
mabus <m.buscaroli@outlook.com>
2022-05-30 19:45:54 +0100

    can now fetch a post by linkId

--- server/api/controllers/posts.js
+++ server/api/controllers/posts.js
@@ -5,13 +5,25 @@ const Post = require('../models/post')
 
 router.post('/', async (req, res) => {
   try {
-    console.log('posts.js - post - req.body -> ', req.body)
+    // console.log('posts.js - post - req.body -> ', req.body)
     const post = await Post.create(req.body)
-    console.log('posts.js - post - post -> ', post)
-    res.status(201).send(post)
+
+    // console.log('posts.js - post - post -> ', post)
+    res.status(201).send(JSON.stringify(post))
   } catch (err) {
     // 406 - Not Acceptable
     res.status(406).json({ err })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    console.log('posts.js - get /:id - req.params.id -> ', req.params.id)
+    const retrievedPost = await Post.getPostByLinkId(req.params.id)
+    const postToSend = JSON.stringify(retrievedPost)
+    res.send(postToSend)
+  } catch (err) {
+    res.status(404).send({ error: 'Post not found.' })
   }
 })
 
--- server/api/models/post.js
+++ server/api/models/post.js
@@ -14,19 +14,36 @@ class Post {
     return new Promise(async (resolve, reject) => {
       try {
         let link = uuidv4().toString()
-        console.log(
-          `post.js create -> title: ${title}, username: ${username}, story: ${story}, link: ${link} `
-        )
+        // console.log(
+        //   `post.js create -> title: ${title}, username: ${username}, story: ${story}, link: ${link} `
+        // )
         let newPostData = await db.query(
           `INSERT INTO posts (title, username, story, link) VALUES ($1, $2, $3, $4) RETURNING *;`,
           [title, username, story, link]
         )
-        console.log('post.js - create - newPostData -> ', newPostData)
+        // console.log('post.js - create - newPostData -> ', newPostData)
         let newPost = new Post(newPostData.rows[0])
-        console.log('post.js - create - newPost -> ', newPost)
+        // console.log('post.js - create - newPost -> ', newPost)
         resolve(newPost)
       } catch (err) {
         reject('Could not create the post.')
+      }
+    })
+  }
+
+  static getPostByLinkId(link) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let targetPost = await db.query(
+          `SELECT * FROM posts WHERE link=($1);`,
+          [link]
+        )
+
+        const postToSend = targetPost.rows[0]
+        console.log('post.js - getPostByLinkId - postToSend -> ', postToSend)
+        resolve(postToSend)
+      } catch (err) {
+        reject('Post not found.')
       }
     })
   }
