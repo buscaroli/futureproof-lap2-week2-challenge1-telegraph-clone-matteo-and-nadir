768240ef5ad5808e04a7aca7a296141c19b6cf66
mabus <m.buscaroli@outlook.com>
2022-05-31 11:00:22 +0100

    add selectors to index.js

--- client/static/index.js
+++ client/static/index.js
@@ -1,1 +1,11 @@
-const app = require("./app");
+const app = require('./app')
+
+const form = document.querySelector('form')
+const title = document.querySelector('#title')
+const name = document.querySelector('#name')
+const story = document.querySelector('#story')
+const submitBtn = document.querySelector('#publish')
+
+const pathArray = window.location.pathname.split('/')
+const linkId = pathArray[-1]
+console.log(linkId)
