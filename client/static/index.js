826e1ef6f2a49065b31d7f8b68f1922148eaf028
mabus <m.buscaroli@outlook.com>
2022-05-31 22:02:33 +0100

    app working

--- client/static/index.js
+++ client/static/index.js
@@ -1,28 +1,103 @@
-const form = document.querySelector('form')
-const title = document.querySelector('#title')
-const username = document.querySelector('#name')
-const story = document.querySelector('#story')
+const formEl = document.querySelector('form')
+const titleEl = document.querySelector('#title')
+const usernameEl = document.querySelector('#name')
+const storyEl = document.querySelector('#story')
 const submitBtn = document.querySelector('#publish')
-const date = document.querySelector('#date')
+const dateEl = document.querySelector('#date')
 
-const pathArray = window.location.pathname.split('/')
-const linkId = pathArray[pathArray.length - 1]
-console.log(linkId)
+window.onload = onLoad
 
-// window.onload = function (e) {
-//   e.preventDefault()
-//   alert(linkId)
-//   getPost(linkId)
-// }
-
-window.addEventListener('mousedown', function (e) {
+formEl.addEventListener('submit', (e) => {
   e.preventDefault()
+  addPost(titleEl.value, usernameEl.value, storyEl.value)
+})
+
+function onLoad(e) {
+  e.preventDefault()
+
+  const address = window.location.href.split('?')
+  console.log('address.length', address.length)
+  const linkId = address[address.length - 1]
   console.log(linkId)
-  getPost(linkId)
-})
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const dataToDisplay = addPost(title.value, username.value, story.value)
-  console.log('dataToDisplay: ', dataToDisplay)
-})
+  if (address.length > 1) {
+    getPost(linkId)
+  }
+}
+
+function checkDataIsValid() {
+  if (titleEl.value !== '' && usernameEl.value !== '' && storyEl.value !== '')
+    return true
+  return false
+}
+
+function addPost(title, username, story) {
+  const newPost = {
+    title,
+    username,
+    story,
+    link: '',
+  }
+
+  console.log(newPost)
+
+  if (checkDataIsValid()) {
+    fetch('http://localhost:3000/posts/', {
+      method: 'POST',
+      body: JSON.stringify(newPost),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log('sent data, link is ', data.link)
+        console.log('data -> ', data)
+        // populateFields(post.title, post.username, post.story, post.link)
+        // window.location.href = `${window.location.protocol}//${window.location.host}/${data.link}`
+        // window.location.reload()
+        // populateFields(data)
+        window.location.href = window.location.href + '?' + data.link
+        console.log('protocol', window.location.protocol)
+        console.log('hostname', window.location.hostname)
+        console.log('port', window.location.port)
+        console.log('href', window.location.href)
+        console.log('ffff', window.location.href + '?' + data.link)
+      })
+  } else {
+    alert('Please fill all fields!')
+  }
+}
+
+function getPost(linkId) {
+  console.log(`linkId is ${linkId.length} chars long`)
+  console.log(location.href)
+  fetch(`http://localhost:3000/posts/${linkId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      populateFields(data)
+    })
+  // .then((post) => {
+  //   console.log('*********** ', post)
+
+  //   // let url = window.location.href
+  //   // let newUrl = url + `?${linkId}`
+  //   // window.open(newUrl, '_self')
+  // })
+}
+
+function populateFields({ title, username, story, enteredat }) {
+  titleEl.value = title
+  usernameEl.value = username
+  storyEl.value = story
+  dateEl.textContent = enteredat
+}
+
+function toggleForm(elem) {
+  if (elem.display === 'flex') {
+    elem.display = 'none'
+  } else {
+    elem.display = 'flex'
+  }
+}
